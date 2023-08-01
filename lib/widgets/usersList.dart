b345fcc94f9749a20ed8fcd5426831c70a625a4f
HibaADJ <adjmihibat@gmail.com>
2023-07-31 21:08:44 -0400

    first commit

---
+++ lib/widgets/usersList.dart
@@ -0,0 +1,25 @@
+import 'package:affinity_game/functions/favHandleTap.dart';
+import 'package:flutter/material.dart';
+import 'package:affinity_game/data/user.data.dart';
+
+import '../data/user.data.dart';
+import 'card.dart';
+import 'favoriteCard.dart';
+class UsersList2 extends StatelessWidget {
+  const UsersList2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      // Let the ListView know how many items it needs to build.
+      itemCount: listUsers.length,
+      // Provide a builder function. This is where the magic happens.
+      // Convert each item into a widget based on the type of item it is.
+      itemBuilder: (context, index) {
+        final item = listUsers[index];
+
+        return UserCard(user: item);
+      },
+    );
+  }
+}
